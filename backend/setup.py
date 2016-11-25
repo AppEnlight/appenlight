349cfd8a5d57959e8baceeadb51db3cdb7837eba
Marcin Lulek <info@webreactor.eu>
2016-11-25 14:04:04 +0100

    readme: updated readme

--- backend/setup.py
+++ backend/setup.py
@@ -5,7 +5,7 @@ import re
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = open(os.path.join(here, '..', 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGELOG.rst')).read()
 
 REQUIREMENTS = open(os.path.join(here, 'requirements.txt')).readlines()
